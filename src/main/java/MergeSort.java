2f58928119bb3414d71658f32e51f1b09f1dd80f
DJRoche509 <davjroche@gmail.com>
2023-07-03 15:53:55 -0400

    Update file with comment about runtime complexity

--- src/main/java/MergeSort.java
+++ src/main/java/MergeSort.java
@@ -2,6 +2,27 @@ package main.java;
 
 import java.util.Arrays;
 
+
+/**
+ * Insertion sort:  Efficient for small inputs or when input is already partially sorted. Not recommended for large or unsorted inputs
+ * Best case: O(n), when the input is already sorted.
+ * Worst case: O(n^2), when the input is sorted in reverse order.
+ * Average case: O(n^2), for random inputs.
+ *
+ *
+ * Merge sort:  Stable and efficient sorting algorithm that works well for large inputs. Time complexity is always O(n log n) regardless of the distribution of the input data
+ * Best case: O(n log n), for all inputs.
+ * Worst case: O(n log n), for all inputs.
+ * Average case: O(n log n), for all inputs.
+ *
+ *
+ * Quicksort:  Fast and efficient sorting algorithm that works well for large inputs and is commonly used in practice.
+ *             However, worst-case time complexity is quadratic,
+ * Best case: O(n log n), when the partitioning is balanced.
+ * Worst case: O(n^2), when the partitioning is unbalanced (e.g., when the pivot is the minimum or maximum element).
+ * Average case: O(n log n), for random inputs.
+ */
+
 public class MergeSort {
     public static void mergeSort(int[] arr) {
         if (arr == null || arr.length <= 1) {
